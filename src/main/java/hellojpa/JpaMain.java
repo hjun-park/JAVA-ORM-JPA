336ca8b642742596a84104d84476976e5e24ae04
hjun-park <phj0860@gmail.com>
2021-04-14 23:46:45 +0900

    [2021-04-14]
    1. 객체와 테이블 매핑
    2. 필드와 컬럼 매핑
    3. 데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,11 +19,10 @@ public class JpaMain {
         try {
 
             // 영속
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-
-//            em.persist(member1);
-//            em.persist(member2);       // 영속성 컨텍스트에 넣고 아래에 커밋
+            // Member member1 = new Member(150L, "A");
+            // Member member2 = new Member(160L, "B");
+            // em.persist(member1);
+            // em.persist(member2);       // 영속성 컨텍스트에 넣고 아래에 커밋
                                        // 커밋을 넣을 때 쿼리가 나간다.
 
 
@@ -35,21 +34,18 @@ public class JpaMain {
                5. flush하고 커밋
              */
 
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");    // 수정 시 영속성 컨텍스트에 들어있기 때문에 persist 안 써도 된다.
+            // Member member = em.find(Member.class, 150L);
+            // member.setName("ZZZZZ");    // 수정 시 영속성 컨텍스트에 들어있기 때문에 persist 안 써도 된다.
                                         // 수정이 되면 알아서 update 쿼리를 보내준다. ( 변경감지 : dirty checking )
 
 
-
-
-
-
+            // ===================================================================================================
             // // 001 비영속, 영속
             // // 비영속 상태  ( JPA와 상관없음 )
             // Member member = new Member();
             // member.setId(101L);
             // member.setName("HelloJPA");
-            //
+
             // // 영속 ( 객체를 저장한 상태 :: 엔티티 매니저 통해서 관리가 됨 )
             // // 사실 persist 썼다고 해서 DB에 바로 저장되는 건 아니다. 커밋을 해야 저장이 된다.
             // // 영속성 컨텍스트 안에 있는 1차 캐시에 저장이 되고 커밋을 하게되면 DB에 들어가게 된다.
@@ -59,19 +55,24 @@ public class JpaMain {
             // // 쿼리가 나가지 않고 1차 캐시에 있는 정보를 가지고 출력되는 것을 볼 수 있다.
             // Member findMember = em.find(Member.class, 101L);
 
+            // ===================================================================================================
+
             // 002 엔티티 매니저
             // 같은 ID를 여러 번 조회해도 쿼리는 1번만 나가게 된다.
             // DB에서 가져와서 영속성 컨텍스트 1차 캐시에 올리기 때문에 한 번 가져오면 그 이후는 가져올 필요가 없기 때문
 
-//            em.detach(member);  // 영속성 컨텍스트를 다시 지움 ( 준영속 )
-//            em.remove(member);  // 실제 DB에서 지우는 것
+            // em.detach(member);  // 영속성 컨텍스트를 다시 지움 ( 준영속 )
+            // em.remove(member);  // 실제 DB에서 지우는 것
 
+
+            // ===================================================================================================
             // 003 데이터 삽입
             // Member member = new Member();
             // member.setId(2L);
             // member.setName("HelloB");
             // em.persist(member);
 
+            // ===================================================================================================
             // 004 데이터 수정 ( Entity Manager 통해서 데이터 가져오면 JPA가 관리하게 됨 )
 //            Member findMember = em.find(Member.class, 1L);
 //            System.out.println("findMember.id = " + findMember.getId());
@@ -79,6 +80,7 @@ public class JpaMain {
 //            findMember.setName("helloJPA");
             // 수정하고 em.persist 할 필요없이 setName만으로도 값이 변경됨
 
+            // ===================================================================================================
 
             // 005 JPQL을 이용한 데이터 수정
             // JPQL은 엔티티 객체 대상으로 쿼리,  SQL은 DB 테이블을 대상으로 쿼리
@@ -93,6 +95,7 @@ public class JpaMain {
 
             // 데이터 삭제
             // em.remove(findMember);
+            // ===================================================================================================
 
             tx.commit();    // 한 트랜잭션이 끝난 후 커밋까지
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,8 +1,9 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 
 @Entity             // JPA 로딩될 때 JPA 사용하는 애구나 관리해야겠다 라고 인식
@@ -10,31 +11,39 @@ import javax.persistence.Table;
 public class Member {
 
     @Id         // id는 Primary key 라고 알려줌
-    private Long id;
-    private String name;
+    private Long id;                    // primary name
+
+    // @Column(unique = true, length = 10) // unique 제약조건 + 최대길이 10
+    // @Column(nullable = false) // NOT NULL
+    // @Column(precision.scale) // 큰 숫자나 소숫점 사용
+    @Column(name = "name", insertable = true, updatable = true) // 등록하고 변경이 가능한지
+    private String username;            // ( 컬럼명이 name )
+
+    private Integer age;                // INT
+
+    @Enumerated(EnumType.STRING)        // enum은 ORDINAL 사용 X, 값이 아닌 순서를 저장하기 때문에.
+    private RoleType roleType;          // DB에는 enum이 없기 때문에 Enumerated 어노테이션 사용
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;            // 날짜 타입 ( 생성일자 ) (( 날짜, 시간, 타임스탬프(날짜+시간)를 DB에서 사용해서 선택 필요 ))
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;      // 날짜 타입 ( 수정일자 )
+
+    // hibernate 에서 이렇게 써도 인식함 ( 최신 버전에서의 사용 )
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;         // varchar를 넘어서는 큰 컨텐츠를 넣기 위해서는 Lob을 이용한다.
+
+    @Transient
+    private int temp;                   // DB와 관련없이 메모리에만 쓰고 싶다. ( 실제로 퀄리로 전송되지 않음 )
+
 
     /* JPA는 기본적으로 내부적으로 reflection 을 쓰기 때문에 기본 생성자 필요 */
     public Member() {
-
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
